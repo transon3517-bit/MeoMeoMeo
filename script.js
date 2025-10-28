aa0daf56dc0c70ed2a7f4707772a9762cf5a255b
Tyson Tran <tysontran@Tysons-MacBook-Pro-M4.local>
2025-10-28 16:55:54 +1100

    Update website: modify HTML, CSS, JS and add images

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const button = document.getElementById("changeColorBtn");
+const heading = document.querySelector("h1");
+
+const color =[
+  {hex: "#f8b400", name: "Yellow"},
+  {hex: "#f44336", name: "Red"},
+  {hex: "#4caf50", name: "Green"},
+  {hex: "#2196f3", name: "Blue"},
+  {hex: "#9c27b0", name: "Purple"}
+];
 
 button.addEventListener("click", () => {
-  const colors = ["#f8b400", "#f44336", "#4caf50", "#2196f3", "#9c27b0"];
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
-  document.body.style.backgroundColor = randomColor;
+  const randomIndex = Math.floor(Math.random() * color.length);
+  const randomColor = color[randomIndex];
+
+  document.body.style.backgroundColor = randomColor.hex;
+
+   heading.textContent = `It is ${randomColor.name}!`;
+  alert("Wow! Did you just changed the color? Let see what color it is!");
+
 });
+
+
